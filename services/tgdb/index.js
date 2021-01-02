15328cbc647cd7c7fbec8d8ebb5122c3a4ebb136
balatzar <bataprod@gmail.com>
2021-01-02 13:45:16 +0100

    Request combined_credits

--- services/tgdb/index.js
+++ services/tgdb/index.js
@@ -1,6 +1,6 @@
 const fetchCredits = async (id) => {
   return await fetch(
-    `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.TGDB_API_KEY}`
+    `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${process.env.TGDB_API_KEY}`
   );
 };
 
