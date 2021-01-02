8da8c82f8e6b29cc078e046de5e2528c5ddaf065
balatzar <bataprod@gmail.com>
2021-01-02 14:35:10 +0100

    Make jobs an array

--- pages/api/credits/[first_person]/[second_person]/index.js
+++ pages/api/credits/[first_person]/[second_person]/index.js
@@ -1,5 +1,60 @@
 import { fetchCredits } from "../../../../../services/tgdb";
 
+const mergeDuplicates = (credits) => {
+  console.log(credits);
+
+  return Object.values(
+    credits.reduce((acc, credit) => {
+      if (acc[credit.id]) {
+        if (
+          !acc[credit.id].first_person_roles.find(
+            ({ job }) => job === credit.first_person_job
+          )
+        ) {
+          acc[credit.id].first_person_roles.push({
+            character: credit.first_person_character,
+            department: credit.first_person_department,
+            job: credit.first_person_job,
+          });
+        }
+        if (
+          !acc[credit.id].second_person_roles.find(
+            ({ job }) => job === credit.second_person_job
+          )
+        ) {
+          acc[credit.id].second_person_roles.push({
+            character: credit.second_person_character,
+            department: credit.second_person_department,
+            job: credit.second_person_job,
+          });
+        }
+      } else {
+        acc[credit.id] = {
+          id: credit.id,
+          title: credit.title,
+          release_date: credit.release_date,
+          poster_path: credit.poster_path,
+          first_person_roles: [
+            {
+              character: credit.first_person_character,
+              department: credit.first_person_department,
+              job: credit.first_person_job,
+            },
+          ],
+          second_person_roles: [
+            {
+              character: credit.second_person_character,
+              department: credit.second_person_department,
+              job: credit.second_person_job,
+            },
+          ],
+        };
+      }
+      return acc;
+    }, {})
+  );
+};
+
 export default async (req, res) => {
   const { first_person, second_person } = req.query;
 
@@ -16,8 +71,8 @@ export default async (req, res) => {
     if (!secondCredit) return acc;
     acc.push({
       id: credit.id,
-      title: credit.title,
-      release_date: credit.release_date,
+      title: credit.title || credit.original_name,
+      release_date: credit.release_date || credit.first_air_date,
       poster_path: credit.poster_path,
       first_person_character: credit.character,
       first_person_department: credit.department,
@@ -29,9 +84,11 @@ export default async (req, res) => {
     return acc;
   };
 
-  const data = firstPersonCredits.cast
+  const allData = firstPersonCredits.cast
     .reduce(compareCredit, [])
     .concat(firstPersonCredits.crew.reduce(compareCredit, []));
+
+  const data = mergeDuplicates(allData);
 
   res.status(200).json({
     data,
