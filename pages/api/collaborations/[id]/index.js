c618b88b322b5677adfeb398e3a00998ca503299
balatzar <bataprod@gmail.com>
2021-01-02 23:53:31 +0100

    Remove toxic returns

--- pages/api/collaborations/[id]/index.js
+++ pages/api/collaborations/[id]/index.js
@@ -21,18 +21,18 @@ export default async (req, res) => {
       mediaCredit.media_type
     );
 
-    if (!mediaRes) return;
+    if (!mediaRes) continue;
 
     const media = await mediaRes.json();
 
-    if (media.status_code === 34) return;
+    if (media.status_code === 34) continue;
     console.log(media.name || media.title);
 
     const fullCollab = media.credits.cast.concat(media.credits.crew);
 
     for (let ii = 0; ii < fullCollab.length; ii++) {
       const collab = fullCollab[ii];
-      if (collab.id === id) return;
+      if (collab.id === id) continue;
       const newMedia = {
         title: media.title || media.name,
         release_date: media.release_date || media.first_air_date,
